743f5783e86eb7d2f7d0b4f60d2b1250b32e1b23
jhael07fmp <jhrodriguez@fmp.com.do>
2024-07-11 16:33:35 -0400

    export in index of the types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 import QFilterBuilder from "./QFilterBuilder";
 import QFilter from "./QFilter";
 import { generateUID, and, group, not, or, where } from "./utils/operations";
+import * as Types from "./types";
 
 export default QFilterBuilder;
 
-export { QFilterBuilder, QFilter, generateUID, and, group, not, or, where };
+export { QFilterBuilder, QFilter, generateUID, and, group, not, or, where, Types };
