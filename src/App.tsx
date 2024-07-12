0e9a3726d3cc75603db5bcf5450e375da2f5f8d6
jhael07fmp <jhrodriguez@fmp.com.do>
2024-07-12 12:43:57 -0400

    library info

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./index.css";
-import QFilterBuilder from "./lib";
+// import QFilterBuilder from "./lib";
 // import QFilterBuilder from "./lib/QFilterBuilder";
 // import { and, where } from "./lib/utils/groupItems";
 // import { and, group, or, where } from "./lib/utils/groupItems";
 
-type Test = { name: string; age: number; city: string; company?: Company };
-type Company = { name: string; arr: string[]; company2?: Company2 };
-type Company2 = { tin: string };
+// type Test = { name: string; age: number; city: string; company?: Company };
+// type Company = { name: string; arr: string[]; company2?: Company2 };
+// type Company2 = { tin: string };
 const App = () => {
-  const users = [
-    {
-      name: "Jhael",
-      age: 20,
-      city: "DN",
-      company: { name: "Jhael", arr: ["s", "maria", "natilia"] },
-    },
-    { name: "Jhael", age: 21, city: "Santiago" },
-    { name: "Galva", age: 26, city: "SD" },
-    { name: "Galva", age: 26, city: "SDE" },
-    { name: "Thomas", age: 20, city: "SDN" },
-    { name: "Sthifer", age: 25, city: "SDN" },
-    { name: "Enmanuel", age: 19, city: "SDO" },
-  ];
-
-  const builder = new QFilterBuilder<Test>()
-    .where("company?.arr.length", "Equal", 0)
-    .or()
-    .where("company", "Equal", undefined);
-
-  // users.filter((item) => item.company.name);
-
-  console.log(builder.build().filter(users));
+  // const users = [
+  //   {
+  //     name: "Jhael",
+  //     age: 20,
+  //     city: "DN",
+  //     company: { name: "Jhael", arr: ["s", "maria", "natilia"] },
+  //   },
+  //   { name: "Jhael", age: 21, city: "Santiago" },
+  //   { name: "Galva", age: 26, city: "SD" },
+  //   { name: "Galva", age: 26, city: "SDE" },
+  //   { name: "Thomas", age: 20, city: "SDN" },
+  //   { name: "Sthifer", age: 25, city: "SDN" },
+  //   { name: "Enmanuel", age: 19, city: "SDO" },
+  // ];
 
   return (
     <div className="w-full h-screen bg-terciary-950 flex justify-center ">
