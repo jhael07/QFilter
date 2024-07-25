4c7e3165b73c0e987a8154f01097708fe537f312
jhael07 <jhaelr007@gmail.com>
2024-07-24 20:24:15 -0700

    exporting css

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,7 @@ import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
 import external from "rollup-plugin-peer-deps-external";
+import css from "rollup-plugin-import-css";
 
 export default [
   {
@@ -16,6 +17,7 @@ export default [
 
     plugins: [
       external(),
+      css(),
       resolve(),
       commonjs(),
       typescript({
