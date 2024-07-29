85239df68a403153a9625980a813de9fede661c3
jhael07fmp <jhrodriguez@fmp.com.do>
2024-07-29 10:37:37 -0400

    qfilter

--- src/components/buttons/CloseButton.tsx
+++ src/components/buttons/CloseButton.tsx
@@ -3,15 +3,11 @@ import { IoClose } from "react-icons/io5";
 import type { CloseButtonProps } from "../../types";
 import { ReactElement } from "react";
 
-
-
 const CloseButton = ({ arr, i, reRenderFn }: CloseButtonProps): ReactElement<any> => {
   const handleDelete = () => {
-    if (arr[i - 1]?.type === "logicalOperator") {
-      arr.splice(i - 1, 2);
-    } else if (arr[i + 1]?.type === "logicalOperator") {
-      arr.splice(i, 2);
-    } else arr.splice(i, 1);
+    if (arr[i - 1]?.type === "logicalOperator") arr.splice(i - 1, 2);
+    else if (arr[i + 1]?.type === "logicalOperator") arr.splice(i, 2);
+    else arr.splice(i, 1);
 
     reRenderFn((prev: boolean) => !prev);
   };
