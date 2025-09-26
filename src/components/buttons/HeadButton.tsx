36dcf08f51a843f2a56c6d9e4811b6dcf97b7517
jhael07fmp <jhrodriguez@fmp.com.do>
2025-09-26 19:19:10 -0400

    build with vite library mode

--- src/components/buttons/HeadButton.tsx
+++ src/components/buttons/HeadButton.tsx
@@ -1,10 +1,10 @@
-import React, { ElementType } from "react";
+import { ButtonHTMLAttributes, ElementType } from "react";
 
 const HeadButton = ({
   title,
   Icon,
   ...rest
-}: { Icon: ElementType; title: string } & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+}: { Icon: ElementType; title: string } & ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button className="q-filter-header-btn button-simple" {...rest}>
       <p className="q-filter-header-text">{title}</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "@jehlicot07/qfilter",
+      fileName: "qfilter",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
